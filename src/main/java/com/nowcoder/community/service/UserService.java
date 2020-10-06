d521ff239b89c273bfbf47555636c410644823b1
qiuke <1138614092@qq.com>
2020-10-06 14:25:23 +0800

    nowcoder_community_code

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -2,8 +2,18 @@ package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailSender;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 
 @Service
@@ -12,7 +22,63 @@ public class UserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private MailSender mailSender;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Value("${community.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
     public User findUserById(int id){
         return userMapper.selectById(id);
+    }
+
+    public Map<String, Object> register(User user){
+        Map<String, Object> map = new HashMap<>();
+
+        // 空值处理
+        if (user == null){
+            throw new IllegalArgumentException("User参数不能为空");
+        }
+
+        if (StringUtils.isBlank(user.getUsername())){
+            map.put("usernameMsg","账号不能为空");
+            return map;
+        }
+        if (StringUtils.isBlank(user.getPassword())){
+            map.put("passwordMsg","密码不能为空");
+            return map;
+        }
+        if (StringUtils.isBlank(user.getEmail())){
+            map.put("emailMsg","邮箱不能为空");
+            return map;
+        }
+
+        User u = userMapper.selectByName(user.getUsername());
+        if (u != null){
+            map.put("usernameMsg","该账号已存在");
+            return map;
+        }
+        u = userMapper.selectByEmail(user.getEmail());
+        if (u != null){
+            map.put("emailMsg","该邮箱已存在");
+            return map;
+        }
+
+        // 注册用户
+        user.setSalt(CommunityUtil.generateUUID().substring(0,6));
+        user.setPassword(CommunityUtil.md5(user.getPassword()+user.getSalt()));
+        user.setType(0);
+        user.setStatus(0);
+        user.setActivationCode(CommunityUtil.generateUUID());
+        user.setHeaderUrl(String.format("http://images.nowcoder.com/head/%dt.png", new Random().nextInt(1000)));
+        user.setCreateTime(new Date());
+
+        return map;
     }
 }
